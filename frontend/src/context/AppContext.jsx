d84009b530f98b9eb4a83863c0a4ed1062ee39a2
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-06-29 16:20:34 +0530

    fetching doctor data to frontend through api call

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,11 +1,40 @@
-import React, { createContext } from 'react';
-import { doctors } from '../assets/assets';
+import React, { createContext, use } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 export const AppContext = createContext();
 
 const AppContextProvider = ( props ) => {
     
     const currencySymbol = '$';
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [doctors, setDoctors] = useState([]);
+
+    const getDoctorsData = async () => {
+
+        try{
+
+            const {data} = await axios.get(backendUrl + '/api/doctor/list');
+            console.log("Doctors data:", data);
+            if(data.success){
+                setDoctors(data.doctors);
+            }
+            else{
+                toast.error(data.message);
+            }
+
+        }
+        catch(error){
+            console.error("Error fetching doctors data:", error);
+            toast.error(error.message);
+        }
+
+    }
+
+    useEffect(() => {
+        getDoctorsData();
+    }, []);
 
     const value = {
         doctors, 
